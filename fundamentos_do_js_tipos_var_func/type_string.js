ed41946d6ecdbb35b503867ca76652f67b0a9be8
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-03 15:41:44 -0300

    Inicio do curso.

---
+++ fundamentos_do_js_tipos_var_func/type_string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Leornado';
+
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template literal
