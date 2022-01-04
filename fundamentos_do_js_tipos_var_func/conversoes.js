2cac38563d0153002c036b1682171afc36ca0d52
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-04 19:51:29 -0300

    Codigos var e conversoes.

---
+++ fundamentos_do_js_tipos_var_func/conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456a";
+
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+console.log(numero + Number(numeroString));
+
+
+// conversao explicita
