54f229696ea7d9deca90a4f62ca1b419f7cca685
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-06 17:58:53 -0300

    Curso finalizado.

---
+++ fundamentos_do_js_tipos_var_func/expressoes.js
@@ -0,0 +1,16 @@
+function minhaFuncao(param){
+    console.log(param)
+}
+
+// minhaFuncao("param")
+
+// console.log(soma(1, 1))
+
+console.log(apresentar())
+
+function apresentar(){
+    return "olá";
+}
+
+const soma = function(num1, num2) { return num1 + num2 }
+console.log(soma(1, 2))
