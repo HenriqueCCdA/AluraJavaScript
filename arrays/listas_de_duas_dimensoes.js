8d653c5f4d9ea6bb351bb3a32efa9588ebe43269
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-07 17:26:31 -0300

    Curso 2 iniciado

---
+++ arrays/listas_de_duas_dimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+
+const mediasDosAlunos = [10, 7, 9, 6];
+
+//let listaDeNotasEAlunos = [alunos, mediasDosAlunos];
+let listaDeNotasEAlunos = [
+    ['João', 'Juliana', 'Caio', 'Ana'],
+    [10, 7, 9, 6]
+];
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}`);
