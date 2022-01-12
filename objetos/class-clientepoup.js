a77ea2bfea3753995a9e6ead5a8da941cfb88adc
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-12 11:10:07 -0300

    Fim do curso 3

---
+++ objetos/class-clientepoup.js
@@ -0,0 +1,37 @@
+class Cliente{
+
+    constructor(nome, email, cpf, saldo){
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+
+    depositar(valor){
+        this.saldo += valor
+    }
+
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
+}
+
+class ClientePouoanca extends Cliente{
+    constructor(nome, email, cpf, saldo, saldoPoupanca){
+        super(nome, email, cpf, saldo)
+        this.saldoPoupanca = saldoPoupanca
+    }
+
+    depositarPoupanca(valor){
+        this.saldoPoupanca += valor
+    }
+}
+
+const andre = new ClientePouoanca("Andre", "a@email.com", "2255887744", 100, 200)
+
+console.log(andre)
+
+andre.depositar(50)
+andre.depositarPoupanca(50)
+
+console.log(andre)
