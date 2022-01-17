a40ee9e555065be4756b39b0024590c617b8cb1f
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-17 17:55:02 -0300

    Regex.

--- nodejs_criando_biblioteca/lib-markdown/index.js
+++ nodejs_criando_biblioteca/lib-markdown/index.js
@@ -1,6 +1,17 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^s#\s].[^\s]*)\)/gm
+    const arrayResultados = []
+
+    let temp
+    while( (temp = regex.exec(texto)) !== null){
+        arrayResultados.push({ [temp[1]]: temp[2]})
+    }
+    return arrayResultados
+}
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
@@ -9,29 +20,10 @@ async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8'
     try{
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinks(texto))
     } catch(erro){
         trataErro(erro)
     }
 }
-
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8'
-//     fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(chalk.green(texto)))
-//     .catch((erro) => trataErro(erro))
-// }
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encodig = 'utf-8'
-//     fs.readFile(caminhoDoArquivo, encodig, (erro, texto) => {
-//         if (erro){
-//             trataErro(erro)
-//         }
-//         console.log(chalk.green(texto))
-//     })
-// }
 
 pegaArquivo('./arquivos/texto1.md')
