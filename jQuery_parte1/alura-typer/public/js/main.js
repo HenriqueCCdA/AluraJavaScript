8be2aaf920d32c083e8cca020ff24bfdbae4c0bb
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-29 13:30:32 -0300

    Implementando o contador.

--- jQuery_parte1/alura-typer/public/js/main.js
+++ jQuery_parte1/alura-typer/public/js/main.js
@@ -3,3 +3,31 @@ let numeroDePalavras = frase.split(" ").length;
 let tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numeroDePalavras);
+
+let campo = $(".campo-digitacao");
+
+campo.on("input", function(){
+    let conteudo = campo.val();
+
+    let qtdPalavras = conteudo.split(/\s+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    let qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+});
+
+
+let tempoRestante = $("#tempo-digitacao").text();
+
+campo.one("focus", function(){
+    var cronametroId = setInterval(
+        function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante < 1){
+                campo.attr("disabled", true)
+                clearInterval(cronametroId);
+            }
+        }, 1000);
+});
