78970d024e3d0d577f17c3ea6d3c7f24528194d5
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-30 13:02:25 -0300

    Add estilos.

--- jQuery_parte1/alura-typer/public/js/main.js
+++ jQuery_parte1/alura-typer/public/js/main.js
@@ -1,33 +1,74 @@
-let frase = $(".frase").text();
-let numeroDePalavras = frase.split(" ").length;
-let tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numeroDePalavras);
-
+let tempoIncial = $("#tempo-digitacao").text();
 let campo = $(".campo-digitacao");
 
-campo.on("input", function(){
-    let conteudo = campo.val();
-
-    let qtdPalavras = conteudo.split(/\s+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    let qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
+function atualizaTamanhoFrase(){
+    let frase = $(".frase").text();
+    let numeroDePalavras = frase.split(" ").length;
+    let tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numeroDePalavras);
+}
 
-let tempoRestante = $("#tempo-digitacao").text();
+function inicializaContadores(){
+    campo.on("input", function(){
+        let conteudo = campo.val();
 
-campo.one("focus", function(){
-    var cronametroId = setInterval(
-        function(){
-            tempoRestante--;
-            $("#tempo-digitacao").text(tempoRestante);
-            if(tempoRestante < 1){
-                campo.attr("disabled", true)
-                clearInterval(cronametroId);
-            }
-        }, 1000);
-});
+        let qtdPalavras = conteudo.split(/\s+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        let qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+
+    });
+}
+
+function inicializaMarcadores(){
+    let frase = $(".frase").text();
+    campo.on("input", function(){
+        let digitado = campo.val();
+        let comparavel = frase.substr(0, digitado.length);
+        if( digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }else{
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+
+    });
+}
+function inicializaCronometro(){
+    let tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
+        var cronametroId = setInterval(
+            function(){
+                tempoRestante--;
+                $("#tempo-digitacao").text(tempoRestante);
+                if(tempoRestante < 1){
+                    campo.attr("disabled", true)
+                    clearInterval(cronametroId);
+                    campo.toggleClass("campo-desativado");
+                }
+            }, 1000);
+    });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val('');
+
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
+    $("#tempo-digitacao").text(tempoIncial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
