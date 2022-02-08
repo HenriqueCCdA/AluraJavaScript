b6ebeb7b2258a6928965e2092ee89f59f188a09a
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-02-08 14:44:07 -0300

    VIews.

--- padroes_de_projeto/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ padroes_de_projeto/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,26 +7,32 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacaoView($('#negociacoesView'));
+
+        this._negociacoesView.update();
 
     }
 
     adiciona(event){
-
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao);
+        this._limpaFormulario();
+    }
 
-        let negociacao = new Negociacao(
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        )
+        );
+    }
 
-        console.log(negociacao);
-
-
-        let diaMesAno = DateHelper.dataPataTexto(negociacao.data);
-
-        console.log(diaMesAno);
-        // adicionar a negocai em um lista
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 
 }
